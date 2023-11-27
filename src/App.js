913a1c86adc44b15b3121419c3f17fdb41840968
Toufiqh92 <toufiqh12@gmail.com>
2023-11-27 01:02:31 -0500

    New Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import OpenAI from 'openai';
+import { OPENAI_API_KEY } from '.env';
 
+dotenv.load();
+const port = process.env.PORT;
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const chatCompletion = await openai.chat.completions.create({
+messages: [{ role: "user", content: "Say this is a test" }],
+model: "gpt-3.5-turbo",
+
+});
+console.log(chatCompletion);
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+      <h1>
+        
+      </h1>
+     </div>
     </div>
   );
 }
