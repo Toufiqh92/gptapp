7c147f7d3c3b1d41df88fdfe46728d5f7fb8c47a
Toufiqh92 <toufiqh12@gmail.com>
2023-11-29 11:29:04 -0500

    gitignore

--- src/Api/server.py
+++ src/Api/server.py
@@ -1,11 +1,16 @@
 from flask import Flask, redirect,url_for,render_template
+import openai 
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 app = Flask(__name__)
+
 
 @app.route("/app.js")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
